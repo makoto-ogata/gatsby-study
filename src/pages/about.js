87821c71b49bfd1d2e3a113c1d592f4b5cb4b395
Makoto Ogata <ogatamakoto1976@gmail.com>
2020-07-08 22:51:45 +0900

    add font awesome

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,94 +5,46 @@ import Layout from "../components/layout"
 
 export default ({ data }) => (
   <Layout>
-    <section className="hero">
+    <div className="eyecatch">
       <figure>
-        <Img fluid={data.hero.childImageSharp.fluid} alt="" style={{ height: "100%" }} />
+        <Img fluid={data.about.childImageSharp.fluid} alt="ブルーベリー＆ヨーグルト" />
       </figure>
-      <div className="catch">
-        <h1>There is no love sincerer than<br /> the love of food.</h1>
-        <p>食物を愛するよりも誠実な愛はない ― バーナード・ショー</p>
-      </div>
-      <div className="wave">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1366 229.5" fill="#fff"><path d="M1369,6.3C1222.5-12.2,1189.5,8,919.2,96.6C665,179.8,160,141.7-2,53.1v150l1371-14.2V6.3z" opacity=".53" /><path d="M1369 229.5V55.8c-9.5-2.4-19.2-4.4-28.9-5.8-196.9-29.9-203.4-15.8-503.9 82.6-219.8 72-627.6 53.2-838.2-10.5v107.4h1371z" /></svg>
-      </div>
-    </section>
-    <section className="food">
+    </div>
+    <article className="content">
       <div className="container">
-        <h2 className="bar">Food <span>Essence</span></h2>
-        <div className="details">
-          <div className="detail">
-            <figure>
-              <Img fixed={data.fruit.childImageSharp.fixed} alt="" />
-            </figure>
-            <h3>フルーツ</h3>
-            <p>FRUIT</p>
-            <p>甘くてすっぱくておいしい果実たち。<br />旬のフルーツを満喫します。</p>
+        <h1 className="bar">ESSENTIALSについて</h1>
+        <aside className="info">
+          <div className="subtitle">
+            <i className="fas fa-utensils" />
+            ABOUT ESSENTIALS
           </div>
-          <div className="detail">
-            <figure>
-              <Img fixed={data.grain.childImageSharp.fixed} alt="" />
-            </figure>
-            <h3>穀物</h3>
-            <p>GRAIN</p>
-            <p>食事の基本となる穀物。<br />毎日の活動のエネルギー源になります。</p>
-          </div>
-          <div className="detail">
-            <figure>
-              <Img fixed={data.beverage.childImageSharp.fixed} alt="" />
-            </figure>
-            <h3>飲み物</h3>
-            <p>BEVERAGE</p>
-            <p>リラックスするのに欠かせない飲み物。<br />お気に入りの一杯はありますか？</p>
-          </div>
+        </aside>
+        <div className="postbody">
+          <p>体に必要不可欠な食べ物についての情報を発信しているサイトです。「おいしい食材をおいしく食べる」をモットーにしています。特に力を入れているのが、フルーツ、穀物、飲み物の３つです。</p>
+          <h2><i className="fas fa-check-square" />公開している記事</h2>
+          <p>これらについて、次のような記事やレシピなどを書いて公開しています。</p>
+          <ul>
+            <li>ヘルシーで美味しい料理の研究・レビュー・レシピ。</li>
+            <li>一人でも、家族や友人と一緒にでも楽しめる料理。</li>
+            <li>ユーザー間のオープンなコミュニケーションを基盤とした情報。</li>
+          </ul>
+          <h2><i className="fas fa-check-square" />よく聞かれること</h2>
+          <p>よく聞かれることなのですが、私たちはスタートアップではありません。私たちはまだスタートしたばかりで、より多くの人々が食べやすい食べ物がもたらす違いを発見できるように、成長しサービスを改善する機会を待っています。</p>
+          <p>気長にお付き合いいただければ幸いです。</p>
         </div>
       </div>
-    </section>
-    <section className="photo">
-      <h2 className="sr-only">Photo</h2>
-      <figure>
-        <Img fluid={data.berry.childImageSharp.fluid} alt="赤く熟したベリー" style={{ height: "100%" }} />
-      </figure>
-    </section>
+    </article>
   </Layout>
 )
 
 export const query = graphql`
   query {
-  hero: file(relativePath: {eq: "hero.jpg"}) {
-    childImageSharp {
-      fluid(maxWidth: 1600) {
-        ...GatsbyImageSharpFluid_withWebp
+    about: file(relativePath: { eq: "about.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 1600) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
       }
     }
   }
-  fruit: file(relativePath: {eq: "fruit.jpg"}) {
-    childImageSharp {
-      fixed(width: 200) {
-        ...GatsbyImageSharpFixed_withWebp
-      }
-    }
-  }
-  grain: file(relativePath: {eq: "grain.jpg"}) {
-    childImageSharp {
-      fixed(width: 200) {
-        ...GatsbyImageSharpFixed_withWebp
-      }
-    }
-  }
-  beverage: file(relativePath: {eq: "beverage.jpg"}) {
-    childImageSharp {
-      fixed(width: 200) {
-        ...GatsbyImageSharpFixed_withWebp
-      }
-    }
-  }
-  berry: file(relativePath: {eq: "berry.jpg"}) {
-    childImageSharp {
-      fluid(maxWidth: 320) {
-        ...GatsbyImageSharpFluid_withWebp
-      }
-    }
-  }
-}
 `
